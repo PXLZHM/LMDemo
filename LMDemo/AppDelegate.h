7417b8c9193c08f6bf74f009a7aee9ebf7053ed9
xsy <xsy@xsydemacbook-pro.local>
2016-08-30 17:15:00 +0800

    Initial Commit

---
+++ LMDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LMDemo
+//
+//  Created by xsy on 16/8/30.
+//  Copyright © 2016年 pxl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LMDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LMDemo
+//
+//  Created by xsy on 16/8/30.
+//  Copyright © 2016年 pxl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
